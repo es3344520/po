ba113a501d66840afaf542eb4e66603f18972ad7
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-07 20:45:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/get-key', (req, res) => {
-  const key = process.env.KEY_NAME;
-  res.json({ key });
+// 将此替换为你要代理的目标API的URL
+const targetApiUrl = 'https://my-dvgd.onrender.com';
+
+app.use(express.json());
+
+app.post('/proxy', async (req, res) => {
+  try {
+    const response = await axios.post(targetApiUrl, req.body, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.API_SECRET_KEY}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing the request.' });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Proxy server listening at http://localhost:${port}`);
 });
