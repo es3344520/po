f502c8c22cbc3b63ab58cd1470d663ebcd27c21b
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-07 23:04:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 将此替换为你要代理的目标API的URL
-const targetApiUrl = 'https://my-zyvg.onrender.com';
-
 app.use(express.json());
 
-app.post('/proxy', async (req, res) => {
-  try {
-    const response = await axios.post(targetApiUrl, req.body, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.API_SECRET_KEY}`,
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while processing the request.' });
-  }
+app.post('/', (req, res) => {
+  res.json({ message: 'Hello from the target API' });
 });
 
 app.listen(port, () => {
-  console.log(`Proxy server listening at http://localhost:${port}`);
+  console.log(`Target API server listening at http://localhost:${port}`);
 });
