25262f35b4d107d5f08263fb4e59b23da66052a3
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-08 09:29:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.get("/get-password", (req, res) => {
   res.json({ password });
 });
 
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
