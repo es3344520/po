b962ce28257e7a0da44cb0ba06c1ff4a27af781d
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-08 08:33:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.post('/proxy', async (req, res) => {
     const response = await axios.post(targetApiUrl, req.body, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${req.body.apiKey}`, // 使用传递过来的API密钥
+        'Authorization': `Bearer ${process.env.API_SECRET_KEY}`,
       },
     });
     res.json(response.data);
@@ -22,25 +22,6 @@ app.post('/proxy', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while processing the request.' });
   }
 });
-
-// 新的/get-data路由，用于接收来自Tampermonkey脚本的请求
-app.post('/get-data', async (req, res) => {
-  try {
-    const response = await axios.post('/proxy', {
-      ...req.body,
-      apiKey: process.env.API_SECRET_KEY, // 将API密钥作为请求的一部分传递给/proxy路由
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while processing the request.' });
-  }
-});
-
-// 删除不再需要的/get-api-key路由
-// app.get('/get-api-key', (req, res) => {
-//   res.json({ apiKey: process.env.API_SECRET_KEY });
-// });
 
 app.listen(port, () => {
   console.log(`Proxy server listening at http://localhost:${port}`);
