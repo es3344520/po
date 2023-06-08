b5174cab22c99e98eea656d53e7fb2afaff0242b
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-08 22:01:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const atob = require('atob');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const encodedVal = "TUpmYllNeTlqbVp4R2NPZzJTVCt5dDNhdSswR3NjUFFBSUJwT2VvbS9vaz0";
-const customCode = "YXNka2Rvc2lmb213a2ppYQ";
+const encodedVal = process.env.decodedEncVal || "TUpmYllNeTlqbVp4R2NPZzJTVCt5dDNhdSswR3NjUFFBSUJwT2VvbS9vaz0";
+const customCode = process.env.decodedCustomCode || "YXNka2Rvc2lmb213a2ppYQ";
+const AUTHENTICATION_KEY = process.env.AUTHENTICATION_KEY || "your-secret-key";
 
 app.get('/data', async (req, res) => {
+  if (!req.query.key || req.query.key !== AUTHENTICATION_KEY) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+  
   try {
     const decodedEncVal = atob(encodedVal);
     const decodedCustomCode = atob(customCode);
