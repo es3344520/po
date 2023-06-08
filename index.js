90f7c419bc032393123cd7ae2569a2dadbd5ab37
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-08 22:44:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 const encodedVal = process.env.decodedEncVal || "TUpmYllNeTlqbVp4R2NPZzJTVCt5dDNhdSswR3NjUFFBSUJwT2VvbS9vaz0";
 const customCode = process.env.decodedCustomCode || "YXNka2Rvc2lmb213a2ppYQ";
-const AUTHENTICATION_KEY = process.env.AUTHENTICATION_KEY || "your-secret-key";
+const MY_CUSTOM_ACCESS_TOKEN = process.env.MY_CUSTOM_ACCESS_TOKEN || "your-secret-key";
 
 app.get('/data', async (req, res) => {
-  if (!req.query.key || req.query.key !== AUTHENTICATION_KEY) {
+  if (!req.query.key || req.query.key !== MY_CUSTOM_ACCESS_TOKEN) {
     res.status(401).send('Unauthorized');
     return;
   }
